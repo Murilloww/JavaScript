737990aa7b727ddc01916abdd38e7ef6a5c335c7
Murillo_Lopesss <murillolopes9335@gmail.com>
2024-09-18 20:21:00 -0300

    math operations with select

--- exercicios/ex017/script.js
+++ exercicios/ex017/script.js
@@ -1,1 +1,17 @@
-alert('Ola');
+function calcular(){
+    var numeroTabuada = document.getElementById('txtnum')
+    var tabuada = document.getElementById('selectTabuada')
+
+    if(numeroTabuada.value.length == 0){
+        window.alert('ERRO')
+    } else {
+        tabuada.innerHTML = ''
+        for(var c = 1; c <= 10; c++){
+            var num = Number(numeroTabuada.value)
+            var item = document.createElement('option')
+            item.text = `${num} x ${c} = ${num*c}`
+            item.value = `tab${c}`
+            tabuada.appendChild(item)
+        }
+    }
+}
